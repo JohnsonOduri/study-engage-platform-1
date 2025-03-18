405960e3bd78e9152ee08141e92ec52b9acb456b
JohnsonOduri <oduri.johnson@gmail.com>
2025-03-18 18:50:35 +0530

    add database connectivity

---
+++ src/firebase.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6t8tvChszF5IMvimyICfCkfDNmkp_i_0",
+    authDomain: "edu-connect-cedb9.firebaseapp.com",
+    projectId: "edu-connect-cedb9",
+    storageBucket: "edu-connect-cedb9.firebasestorage.app",
+    messagingSenderId: "35140204969",
+    appId: "1:35140204969:web:a43ed3b9088120b4c2ecb8",
+    measurementId: "G-V6EWS1S986",
+    databaseURL: "https://edu-connect-cedb9-default-rtdb.firebaseio.com"
+  };
+
+
+  
+  export const app = initializeApp(firebaseConfig);
+  export const analytics = getAnalytics(app);
