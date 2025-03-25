f0924a92ecb1459f8f62762bb3cc52f54443af1c
Isaac Roy <isaacroy000@gmail.com>
2025-03-25 07:07:15 +0530

    New
    New

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import { AuthProvider } from '@/contexts/AuthContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+import { defineConfig } from 'vite'
+import React from '@vitejs/plugin-react'
+import path from 'path'
 
 // Create a client for react-query
 const queryClient = new QueryClient()
